3ac2e1e3fa304008366ecf9b168699ba2953546f
jerrycoyne <coyne.jerry@gmail.com>
2017-03-05 22:17:35 -0500

    Update IST311.java

--- src/ist311/IST311.java
+++ src/ist311/IST311.java
@@ -10,22 +10,20 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-/**
- *
- * @author jvl6013
- */
-public class IST311 {
 
-    /**
-     * @param args the command line arguments
-     */
+public class Sandbox {
+
+  
     public static void main(String[] args) {
         LoginCtrl lc1 = new LoginCtrl();
-        //Instantiating LoginUI - Jerry
         LoginUI theLoginUI = new LoginUI();
         lc1.addUser("asdfasdf");
         System.out.println(lc1.authenticate("jvl6012"));
     }
+
+    
+    
+}   
 
     //tedst comment
     
