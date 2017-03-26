52ffee520dceaf845d862eab62e3d4e0ae71a6e5
fivewen <jvl6013@psu.edu>
2017-03-26 16:38:51 -0400

    CalUI2

--- src/ist311/CalUI2.java
+++ src/ist311/CalUI2.java
@@ -5,26 +5,29 @@
  */
 package ist311;
 
-import java.awt.Dimension;
-import java.awt.GridLayout;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
+import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
+
+import static java.awt.GridBagConstraints.PAGE_START;
 
 /**
  *
  * @author jvl6013
  */
 public class CalUI2 extends JFrame{
+    JFrame frame = new JFrame("Table");
+    JPanel panel = new JPanel();
+    JTable table = new JTable();
+    JScrollPane pane = new JScrollPane();
+
     public CalUI2(){
-        JFrame frame = new JFrame("Table");
-        
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
@@ -32,74 +35,83 @@ public class CalUI2 extends JFrame{
         JPanel panel = new JPanel();
         panel = initCal();
         
-        frame.setSize(800, 800);
+        frame.setSize(800, 600);
         frame.add(panel);
     }
     
     private JPanel initCal(){
-        Calendar cal = new GregorianCalendar(2017, 2, 1);
-        int day, month, year;
-        
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(2, 1));
+        CalCtrl calcontrol = new CalCtrl();
+
+        panel.setLayout(new BorderLayout());
         
         JPanel navPanel = new JPanel();
-        navPanel.setLayout(new GridLayout(1, 3));
+        navPanel.setLayout(new FlowLayout());
         
         JPanel textPanel = new JPanel();
         textPanel.setLayout(new GridLayout(2, 1));
         
-        JLabel yearLabel = new JLabel();
-        yearLabel.setText("YEAR: " + cal.get(GregorianCalendar.YEAR));
-        JLabel monthLabel = new JLabel();
-        monthLabel.setText("MONTH: " + (cal.get(GregorianCalendar.MONTH) + 1));
+        JLabel yearLabel = new JLabel("" + calcontrol.getYear(), SwingConstants.CENTER);
+        JLabel monthLabel = new JLabel(calcontrol.toStringtMonth(calcontrol.getMonth()), SwingConstants.CENTER);
         textPanel.add(yearLabel);
         textPanel.add(monthLabel);
         
         JButton prevMonth = new JButton();
         prevMonth.setText("Previous Month");
+        prevMonth.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                panel.remove(pane);
+
+                calcontrol.prevMonth();
+                table = calcontrol.getCalendar();
+                yearLabel.setText("YEAR: " + calcontrol.getYear());
+                monthLabel.setText(calcontrol.toStringtMonth(calcontrol.getMonth()));
+
+                pane = new JScrollPane(table);
+
+
+                pane.revalidate();
+                panel.add(pane);
+
+            }
+        });
         JButton nextMonth = new JButton();
         nextMonth.setText("Next Month");
+        nextMonth.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                panel.remove(pane);
+
+                calcontrol.nextMonth();
+                table = calcontrol.getCalendar();
+                yearLabel.setText("YEAR: " + calcontrol.getYear());
+                monthLabel.setText(calcontrol.toStringtMonth(calcontrol.getMonth()));
+
+                pane = new JScrollPane(table);
+                panel.add(pane);
+
+            }
+        });
+
 
         navPanel.add(prevMonth);
         navPanel.add(textPanel);
         navPanel.add(nextMonth);
         
-        panel.add(navPanel);
+        panel.add(navPanel, BorderLayout.PAGE_START);
+
+        table = calcontrol.getCalendar();
+
+        System.out.println(calcontrol.cal.get(Calendar.DAY_OF_WEEK));
         
-        String[] columnNames = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-        String data[][] = {{}};
-        DefaultTableModel model = new DefaultTableModel(data, columnNames);
-        model.setRowCount(6);
-        model.setColumnCount(7);
+        pane = new JScrollPane(table);
         
-        JTable table = new JTable(model);
-        table.setRowHeight(50);
-        
-        day = cal.get(GregorianCalendar.DAY_OF_MONTH);
-        month = cal.get(GregorianCalendar.MONTH);
-        year = cal.get(GregorianCalendar.YEAR);
-        
-        System.out.println(day + " " + month + " " + year);
-        
-        int som = cal.get(GregorianCalendar.DAY_OF_WEEK);
-        int nod = cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH);
-        System.out.println(som);
-        System.out.println(nod);
-        
-        for (int i=1; i<=nod; i++){
-            int row = new Integer((i+som-2)/7);
-            int column = (i+som-2)%7;
-            table.setValueAt(i, row, column);
-        }
-        
-        JScrollPane pane = new JScrollPane(table);
-        
-        panel.add(pane);
+        panel.add(pane, BorderLayout.CENTER);
         panel.setOpaque(true);
         
         return panel;
     }
-    
     
 }
